3093805e5ea1cdf693c9a77796c92983e6d8b1fb
Jose Pulido <al_jpulidod@iessierradelasvillas.org>
2022-03-22 09:03:37 +0100

    Cambio de variables

--- edades.py
+++ edades.py
@@ -1,4 +1,9 @@
 edadJuan = int(input("Dime la edad de Juan: "))
 edadJose = int(input("Dime la edad de Jose: "))
 
+#Intercambio de las edades
+edadAux = edadJose
+edadJose = edadJuan
+edadJuan = edadAux
+
 print("Juan tiene", edadJuan, "años y Jose tiene", edadJose, "años")
