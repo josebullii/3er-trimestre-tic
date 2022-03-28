7e6cae5127add27fffd46422e087ab984c6320ce
Jose Pulido <al_jpulidod@iessierradelasvillas.org>
2022-03-28 09:48:25 +0200

    5 primeros ejercicios completados

---
+++ Ejercicios Secuenciales/Ejercicio4.py
@@ -0,0 +1,9 @@
+n1 = int(input("Dime un número: "))
+n2 = int(input("Dime otro número: "))
+
+suma = n1 + n2
+resta = n1 - n2
+producto = n1 * n2
+cociente = n1 / n2
+
+print("De ambos números, la suma es", suma, "la resta es", resta, "el producto es", producto, "y el cociente es", cociente)
